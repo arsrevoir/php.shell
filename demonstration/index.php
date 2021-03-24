740f789d0c379f76418203b494666026a942b5ea
arsrevoir <arseniykotliarov@gmail.com>
2021-03-24 20:46:59 +0200

    v0.2

---
+++ demonstration/index.php
@@ -0,0 +1,23 @@
+<?php 
+    require '../main/classes/connection.php';
+
+    $conn = new Connection('localhost', null, 'root', '');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php.shell demonstration</title>
+</head>
+<body>
+    <div class="page">
+        <main role="main">
+            <?php 
+                
+            ?>
+        </main>
+    </div>
+</body>
+</html>
---
+++ main/classes/connection.php
@@ -0,0 +1,55 @@
+<?php 
+
+    // connection related classes
+    class Connection {
+
+        public object $dbh;
+
+        public function __construct($server, $db, $user, $pass) {
+            $server = 'mysql:host=' . $server . ';';
+            ($db) ? $db = 'dbname=' . $db : $db = '';
+
+            $this->connection($server, $db, $user, $pass);
+        }
+
+        //class body 
+        private function connection($server, $db, $user, $pass) {
+            try {
+                $this->dbh = new PDO($server . $db, $user, $pass);
+                $this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                (isset($_SESSION)) : session_start();
+
+            } catch(PDOException $e) {
+                die();
+            }
+        }
+        
+            //request methods
+        public function select($sql, $values) {
+            $sth = $this->dbh->prepare($sql);
+            $sth->execute($values);
+
+            $result = array();
+            while($row = $sth->fetch()) {
+                array_push($result, $row);
+            }
+
+            return $result;
+        }
+
+        public function insert($sql, $values) {
+            $sth = $this->dbh->prepare($sql);
+            $sth->execute($values);
+        }
+
+
+        //connection closure
+        public function __destruct() {
+            $this->dbh == null;
+        }
+    }
+
+    //***$values variable in request methods should be an associative array
+
+?>
